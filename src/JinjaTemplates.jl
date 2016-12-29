21aa67b1ea926e7ae43b3d08e3b664c8a866d817
Dustin Broderick <dbprodigious@gmail.com>
2016-12-29 14:28:19 -0700

    Allows editing of searchpath

--- src/JinjaTemplates.jl
+++ src/JinjaTemplates.jl
@@ -1,6 +1,6 @@
 module JinjaTemplates
 
-export LazyTemplateLoader, get_template_name, render
+export LazyTemplateLoader, get_template_name, render, get_searchpaths, add_searchpath!
 
 using PyCall
 @pyimport jinja2
@@ -17,9 +17,10 @@ type LazyTemplateLoader
         )
         return new(env, Dict())
     end
+    LazyTemplateLoader(path::AbstractString; kwargs...) = LazyTemplateLoader([path]; kwargs...)
 end
 
-""" Returns a jinja template. Onload loads the tempalte once requested """
+""" Returns a jinja template. Onload loads the template once requested """
 function Base.getindex(loader::LazyTemplateLoader, key)
     try
         return loader.templates[key]
@@ -33,13 +34,41 @@ function Base.getindex(loader::LazyTemplateLoader, key)
     end
 end
 
-""" Takes a variable and converts its type into a string fit for the filesystem """
+"""
+Returns an array of the curret template sources
+"""
+get_searchpaths(loader::LazyTemplateLoader) = loader.jinjaenv[:loader][:searchpath]
+
+"""
+Insert location into search path
+
+`at` can be :start, :end, :index
+index is a 1 based index representing the position
+"""
+function add_searchpath!(loader::LazyTemplateLoader, path::AbstractString; at=:start, index=1)
+    searchpath = loader.jinjaenv[:loader]["searchpath"]
+    if is(at, :start)
+        searchpath[:insert](0, path)
+    elseif is(at, :end)
+        searchpath[:append](path)
+    elseif is(at, :index)
+        searchpath[:insert](index-1, path)
+    end
+
+    # Clear cache
+    loader.templates=Dict()
+    return loader
+end
+
+"""
+Takes a variable and converts its type into a string fit for the filesystem
+"""
 function get_template_name(item)
     t = string(typeof(item))
     return replace(t, r"[^\w\.]", s"_")
 end
 
-""" Renders given kwargs into the specified tempalte """
+""" Renders given kwargs into the specified template """
 function render(loader::LazyTemplateLoader, template::AbstractString; kwargs...)
     template = loader[template]
     return template[:render](; kwargs...)
